f9ccb1b881add7267095e5831666c1a4d86964a0
Allen Welch <allenjwelch@gmail.com>
2018-12-09 09:30:35 -0500

    initial branch commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql')
 // const path = require('path');
-
 
 const readConfig = require('read-config');
 const config = readConfig('./config.json');
@@ -12,15 +12,20 @@ const PORT = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
+//Add routes, both API and view
 // app.use(routes);
 
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
 });
 
+// Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {
 //     app.use(express.static("client/build"));
 // }
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cicerowned");
 
 app.listen(PORT, function() {
     // console.log(config.app.bcClientSecret); // testing config file
